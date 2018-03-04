e262a38090a1aa63bef46b73406728526809ed0a
Utkarsh Sharma <utkarsh.mask@gmail.com>
2018-03-04 18:37:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,6 @@
 console.log('Loaded!');
 
-//change main text by JS
 
-var element = document.getElementById('wub');
-element.innerHTML= 'new value';
-
-var pic = document.getElementById('madi');
-var marginLeft = 0;
-    function moveRight() {
-        marginLeft = marginLeft+10;
-        pic.style.marginLeft = marginLeft + 'px' ;
-    }
-
-pic.onclick = function() {
-    var interval = setInterval(moveRight, 100);
-}
 
 //button
 var button = document.getElementById('counter');
