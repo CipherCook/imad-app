2133e9ecf3cb67ec88c6ecb47d04afdd35377ee5
Utkarsh Sharma <utkarsh.mask@gmail.com>
2018-03-04 17:44:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,6 +18,7 @@ pic.onclick = function() {
 
 //button
 var button = document.getElementById('counter');
+var counter = 0;
 button.onClick = function(){
     counter = counter+1;
     var span = document.getEelementById('count');
