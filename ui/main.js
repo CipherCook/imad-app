246655d8882608d40c79bd8a12a46f3e92e8539f
Utkarsh Sharma <utkarsh.mask@gmail.com>
2018-03-05 12:38:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ button.onClick = function(){
     
     var request = new XMLHttpRequest();
     request.onreadystatechange = function() {
-        if (request.readyState === XMLHttpRequest.DONE {
+        if (request.readyState === XMLHttpRequest.DONE) {
             if (request.status === 200){
                 var counter = request.responseText;
                 var span = document.getElementById('count');
@@ -17,6 +17,7 @@ button.onClick = function(){
         }
     }
     
-    counter = counter+1;
-    
+    //mke request
+    request.open('GET', 'http://utkarshmask.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
